037c1ca3c09712fc78b7e66b15e314e12883fcee
Furqan Ali <furqan.ali@emumba>
2024-10-18 16:03:33 +0500

    update in database

--- Order_Service/Models/models.go
+++ Order_Service/Models/models.go
@@ -1,16 +1,21 @@
 package model
 
 type Order struct {
-	OrderID      uint   `gorm:"primaryKey;column:OrderId;autoIncrement" json:"OrderId"`
-	UserId       uint   `gorm:"size:100;not null;column:UserId" json:"UserId"`
-	RestaurantID uint   `gorm:"column:RestaurantId" json:"RestaurantId"`
-	OrderStatus  string `gorm:"size:100;column:OrderStatus" json:"OrderStatus"`
-	TotalBill    uint   `gorm:"size:100;column:TotalBill" json:"TotalBill"`
+	OrderID      uint   `gorm:"primaryKey;column:order_id;autoIncrement" json:"order_id"`
+	UserId       uint   `gorm:"size:100;not null;column:user_id" json:"user_id"`
+	RestaurantID uint   `gorm:"column:restaurant_id" json:"restaurant_id"`
+	OrderStatus  string `gorm:"size:100;column:order_status" json:"order_status"`
+	TotalBill    uint   `gorm:"size:100;column:total_bill" json:"total_bill"`
+	Item         []Item `gorm:"many2many:order_items;foreignKey:order_id;joinForeignKey:order_id;References:item_id;joinReferences:item_id" json:"items"`
+}
+
+type Item struct {
+	ItemId    uint `gorm:"primaryKey;size:100;not null;column:item_id" json:"item_id"`
+	ItemPrice uint `gorm:"column:item_price" json:"item_price"`
 }
 
 type OrderItem struct {
-	OrderID   uint `gorm:"column:OrderId" json:"OrderId"`
-	ItemId    uint `gorm:"size:100;not null;column:ItemId" json:"ItemId"`
-	Quantity  uint `gorm:"column:Quantity" json:"Quantity"`
-	ItemPrice uint `gorm:"column:ItemPrice" json:"ItemPrice"`
+	OrderID  uint `gorm:"primaryKey;column:order_id" json:"order_id"`
+	ItemId   uint `gorm:"primaryKey;column:item_id" json:"item_id"`
+	Quantity uint `gorm:"column:quantity" json:"quantity"`
 }
