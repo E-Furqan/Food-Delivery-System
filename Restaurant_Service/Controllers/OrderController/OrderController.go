ec94eafc07316282b432cfda645883a4e756907c
Furqan Ali <furqan.ali@emumba>
2024-10-21 15:56:47 +0500

    cancel order

--- Restaurant_Service/Controllers/OrderController/OrderController.go
+++ Restaurant_Service/Controllers/OrderController/OrderController.go
@@ -1,6 +1,7 @@
 package OrderController
 
 import (
+	"log"
 	"net/http"
 
 	model "github.com/E-Furqan/Food-Delivery-System/Models"
@@ -55,7 +56,7 @@ func (OrderController *OrderController) ProcessOrder(c *gin.Context) {
 func (OrderController *OrderController) CancelOrder(c *gin.Context) {
 	email, exists := c.Get("Email")
 	if !exists {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "User not authenticated"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Restaurant not authenticated"})
 		return
 	}
 	email, ok := email.(string)
@@ -82,6 +83,8 @@ func (OrderController *OrderController) CancelOrder(c *gin.Context) {
 	}
 
 	if input.RestaurantId != Restaurant.RestaurantId {
+		log.Print(Restaurant.RestaurantId)
+		log.Print(input.RestaurantId)
 		c.JSON(http.StatusBadRequest, gin.H{
 			"Message": "You are not authorized to cancel this order as it belongs to a different restaurant",
 		})
@@ -89,7 +92,7 @@ func (OrderController *OrderController) CancelOrder(c *gin.Context) {
 	}
 
 	input.OrderStatus = "Cancelled"
-	c.JSON(http.StatusBadRequest, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"Message":       "Order cancelled successfully",
 		"Order details": input,
 	})
