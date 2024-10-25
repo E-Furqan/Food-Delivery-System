24ce5b5622eaacbf8f0b38ef4d959783b65301d8
Furqan Ali <furqan.ali@emumba>
2024-10-25 16:25:47 +0500

    auth service working

--- Restaurant_Service/Middleware/middleware.go
+++ Restaurant_Service/Middleware/middleware.go
@@ -46,23 +46,3 @@ func AuthMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
-
-// func RefreshToken(c *gin.Context) {
-// 	var input payload.RefreshToken
-
-// 	if err := c.ShouldBindJSON(&input); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	accessToken, err := utils.RefreshToken(input.RefreshToken, c)
-
-// 	if err != nil {
-// 		log.Fatal("Error while refreshing token; ", err)
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"access_token": accessToken,
-// 		"expires_at":   time.Now().Add(15 * time.Minute).Unix(),
-// 	})
-// }
