2c4b6e5a143ba81cf1d10356b0b2a28d6d54e301
Furqan Ali <furqan.ali@emumba>
2024-10-31 15:25:51 +0500

    adding test cases

---
+++ Restaurant_Service/Repositories/TestRepositories/ConfigTestDatabase.go
@@ -0,0 +1,44 @@
+package database_test
+
+import (
+	"fmt"
+	"log"
+
+	environmentVariable "github.com/E-Furqan/Food-Delivery-System/EnviormentVariable"
+	model "github.com/E-Furqan/Food-Delivery-System/Models"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func TestDatabaseConnection() *gorm.DB {
+	envVar := environmentVariable.ReadEnv()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	var connection_string = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		envVar.HOST, envVar.PORT, envVar.USER, envVar.PASSWORD, "testrestaurant")
+
+	DB, err = gorm.Open(postgres.Open(connection_string), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	err = DB.AutoMigrate(&model.Restaurant{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+
+	err = DB.AutoMigrate(&model.Item{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+	log.Println("Database connection established")
+
+	return DB
+}
