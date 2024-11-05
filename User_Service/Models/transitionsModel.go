abee6479b7ae3a02565067207f19102515a75a1f
Furqan Ali <furqan.ali@emumba>
2024-11-05 17:15:57 +0500

    sm

--- User_Service/Models/transitionsModel.go
+++
@@ -1,9 +0,0 @@
-package model
-
-func GetOrderTransitions() map[string]string {
-	return map[string]string{
-		"Waiting For Delivery Driver": "In for delivery",
-		"In for delivery":             "Delivered",
-		"Delivered":                   "Completed",
-	}
-}
