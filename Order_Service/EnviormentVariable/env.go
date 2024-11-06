361111b0f03251cc440b23a593436445e2e02ef6
Furqan Ali <furqan.ali@emumba>
2024-11-06 16:36:39 +0500

    sm:

--- Order_Service/EnviormentVariable/env.go
+++ Order_Service/EnviormentVariable/env.go
@@ -34,17 +34,17 @@ func ReadEnv() Environment {
 		log.Printf("Error loading .env file: %v", err)
 	}
 
-	envVar.DATABASE_HOST = utils.GetEnv("HOST", "db")
-	envVar.DATABASE_USER = utils.GetEnv("USER1", "furqan")
-	envVar.DATABASE_PASSWORD = utils.GetEnv("PASSWORD", "furqan")
-	envVar.DATABASE_NAME = utils.GetEnv("DB_NAME", "Order")
+	envVar.DATABASE_HOST = utils.GetEnv("DATABASE_HOST", "db")
+	envVar.DATABASE_USER = utils.GetEnv("DATABASE_USER", "furqan")
+	envVar.DATABASE_PASSWORD = utils.GetEnv("DATABASE_PASSWORD", "furqan")
+	envVar.DATABASE_NAME = utils.GetEnv("DATABASE_NAME", "Order")
 	envVar.BASE_URL = utils.GetEnv("BASE_URL", "http://localhost")
 	envVar.Get_Items_URL = utils.GetEnv("Get_Items_URL", "/restaurant/view/menu")
 	envVar.Process_Order_Restaurant_URL = utils.GetEnv("Process_Order_Restaurant_URL", "/restaurant/process/order")
 	envVar.Process_Order_User_URL = utils.GetEnv("Process_Order_User_URL", "/user/process/order")
 	envVar.RESTAURANT_PORT = utils.GetEnv("RESTAURANT_PORT", ":8082")
 	envVar.USER_PORT = utils.GetEnv("USER_PORT", ":8083")
-	portStr := utils.GetEnv("PORT", "5432")
+	portStr := utils.GetEnv("DATABASE_PORT", "5432")
 	envVar.DATABASE_PORT, err = strconv.Atoi(portStr)
 	if err != nil {
 		log.Printf("Error converting PORT to integer: %v", err)
