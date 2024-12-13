c844555eac40978b436f14c26a63a3f3405dd3fe
Furqan Ali <furqan.ali@emumba>
2024-12-13 17:18:32 +0500

    added activites and helper function and clients for palce order workflow

--- workflow/Activity/OrderActivity.go
+++ workflow/Activity/OrderActivity.go
@@ -17,3 +17,17 @@ func (act *Activity) UpdateOrderStatus(RestaurantId uint, order model.CombineOrd
 
 	return "s", nil
 }
+
+// func (act *Activity) UpdateOrderStatus(RestaurantId uint, order model.CombineOrderItem, token string) (string, error) {
+
+// 	var updatedOrder model.UpdateOrder
+// 	updatedOrder.OrderId = order.OrderId
+// 	updatedOrder.OrderStatus = order.OrderStatus
+// 	updatedOrder.UserID = order.UserID
+// 	_, err := act.OrderClient.UpdateOrderStatus(updatedOrder, token)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	return "s", nil
+// }
--- workflow/EnviormentVariable/env.go
+++ workflow/EnviormentVariable/env.go
@@ -42,7 +42,6 @@ func ReadOrderClientEnv() model.OrderClientEnv {
 	OrderClientEnv.BASE_URL = utils.GetEnv("BASE_URL", "http://localhost")
 	OrderClientEnv.UPDATE_ORDER_STATUS_URL = utils.GetEnv("UPDATE_ORDER_STATUS_URL", "/order/update/status")
 	OrderClientEnv.Fetch_OrderStatus_URL = utils.GetEnv("UPDATE_ORDER_STATUS_URL", "/order/fetch/order/status")
-
 	OrderClientEnv.VIEW_ORDERS_URL = utils.GetEnv("USER_ORDERS_URL", "/order/view/orders")
 	OrderClientEnv.VIEW_ORDER_WITHOUT_DRIVER_URL = utils.GetEnv("VIEW_ORDER_WITHOUT_DRIVER_URL", "/order/view/without/driver/orders")
 	OrderClientEnv.ASSIGN_DRIVER_URL = utils.GetEnv("ASSIGN_DRIVER_URL", "/order/assign/diver")
