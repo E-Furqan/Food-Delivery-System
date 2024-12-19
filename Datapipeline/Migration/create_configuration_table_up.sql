b546ad6db9bc51d6f0eb07c58f2c2cc84bfca25b
Furqan Ali <furqan.ali@emumba>
2024-12-19 16:47:39 +0500

    scucessfully connected with google drive client

--- Datapipeline/Migration/create_configuration_table_up.sql
+++ Datapipeline/Migration/create_configuration_table_up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Configuration (
     ConfigId INT PRIMARY KEY,
-    ClientId VARCHAR(255) NOT NULL,
+    ClientId VARCHAR(255) UNIQUE NOT NULL,
     ClientSecret VARCHAR(255) NOT NULL,
     TokenUri VARCHAR(255) NOT NULL,
     AuthUri VARCHAR(255) NOT NULL,
